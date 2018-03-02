6d6c7bf8bfa402a38ebc949160ba66c6c83a2864
GYCHEN <gy.chen@gms.nutc.edu.tw>
2018-03-02 16:24:51 +0800

    add words API service

---
+++ src/service/wordApi.js
@@ -0,0 +1,33 @@
+import apisauce from 'apisauce';
+
+const api = apisauce.create({
+    // TODO get baseUrl from enviroment
+    baseUrl: 'http://127.0.0.1:4416'
+});
+
+/**
+ * find all words stored in the server
+ *
+ * return: promise
+ */
+const findAll = () => api.get('words');
+
+/**
+ * get specific word
+ *
+ * return: promise
+ */
+const get = (id_) => api.get(`words/${id_}`);
+
+/**
+ * put new word to the server
+ *
+ * return: promise
+ */
+const put = ({ name }) => api.post(`words`, { name });
+
+export default {
+  findAll,
+  get,
+  put
+};
