588048e36a0e1d406871ed419161fdd950fc2296
GYCHEN <gy.chen@gms.nutc.edu.tw>
2018-05-20 17:16:11 +0800

    update

--- src/component/Uppy.js
+++ src/component/Uppy.js
@@ -1,9 +1,42 @@
-import React from 'react';
-import Dashboard from 'uppy/lib/react/Dashboard';
-import uppy from '../util/uppy';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Webcam from 'uppy/lib/plugins/Webcam';
 import 'uppy/dist/uppy.css';
+import uppy from '../util/uppy';
 
-// TODO just test , use WebCam later
-const Uppy = () => <Dashboard uppy={uppy} />;
+
+class Uppy extends Component {
+
+    static propTypes = {
+        uppy: PropTypes.object.isRequired
+    }
+
+    static defaultProps = {
+        uppy
+    }
+
+    constructor(props) {
+        super(props);
+
+        this._ref = this._ref.bind(this);
+    }
+
+    componentDidMount() {
+        const { uppy } = this.props;
+
+        uppy.use(Webcam, {
+            target: this._target,
+            mirror: false
+        });
+    }
+
+    _ref(target) {
+        this._target = target;
+    }
+
+    render() {
+        return (<div ref={this._ref}></div>);
+    }
+}
 
 export default Uppy;
